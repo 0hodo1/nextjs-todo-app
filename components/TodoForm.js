d7fb24b2e42531581d8ec2f8dc3a18440a823e26
Ömer Hodo <omerhodo@gmail.com>
2022-03-24 00:21:50 +0300

    todo add operations finished

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,6 +1,27 @@
 import { Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
 
 const TodoForm = () => {
+  const [todo, setTodo] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    if (todo.title == "" || todo.description == "") {
+      return;
+    }
+    const ref = collection(db, "todos");
+    const docRef = await addDoc(ref, { ...todo, createdAt: serverTimestamp() });
+    console.log(docRef.id);
+    setTodo({ title: "", description: "" });
+    alert(`${docRef.id} created!`);
+  };
+
   return (
     <div>
       <Typography
@@ -11,15 +32,28 @@ const TodoForm = () => {
       >
         Add New Todo
       </Typography>
-      <TextField fullWidth label="Title" margin="normal"></TextField>
       <TextField
+        value={todo.title}
+        fullWidth
+        label="Title"
+        margin="normal"
+        onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+      ></TextField>
+      <TextField
+        value={todo.description}
         fullWidth
         label="Description"
         margin="normal"
         multiline
         maxRows
+        onChange={(e) => setTodo({ ...todo, description: e.target.value })}
       ></TextField>
-      <Button variant="outlined" sx={{ mt: 3 }} color="success">
+      <Button
+        variant="outlined"
+        sx={{ mt: 3 }}
+        color="success"
+        onClick={handleClick}
+      >
         Add Todo
       </Button>
     </div>
